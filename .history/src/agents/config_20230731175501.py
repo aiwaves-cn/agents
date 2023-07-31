f4da677ab29c00bcbdd34d8c923d6358dd2517e2
callanwu <wujialongml@gmail.com>
2023-07-31 18:22:06 +0800

    clean code

--- .history/src/agents/config_20230731175501.py
+++
@@ -1,8 +0,0 @@
-{
-    "MIN_CATEGORY_SIM":0.7,
-"TOY_INFO_PATH":['/home/aiwaves/longli/fenxiang/toy_info.json',"/home/aiwaves/longli/fenxiang/bb_info.json"], #子类目相关知识库的路径
-API_KEY = "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"
-PROXY = 'http://127.0.0.1:7000'
-FETSIZE = 5
-MAX_CHAT_HISTORY = 8
-}
