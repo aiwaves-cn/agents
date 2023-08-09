ee9f734c8091d3290aaa59859026479f14d22afa
jekyllxv <hyde_caesar@sina.com>
2023-08-09 17:18:08 +0800

    feat: add package

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='agent',
+    version='0.1',
+    description='chat agent powered by aiwaves',
+    author='aiwaves',
+    packages=find_packages()
+)
