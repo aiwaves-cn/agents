e7eafd15ab77ad9e3a2a7a314b0401c6fd72c194
jianghuyihei <2504702369@qq.com>
2023-09-01 14:36:27 +0800

    update comment

--- src/agents/Environments/base_environment.py
+++ src/agents/Environments/base_environment.py
@@ -1,42 +1,63 @@
-from utils import get_key_history,get_embedding
+from utils import get_key_history, get_embedding
 import torch
 from LLMs.base_LLM import *
 from Memorys import Memory
 
+
 class Environment:
-    def __init__(self,config) -> None:
-        self.shared_memory = {"long_term_memory":[],"short_term_memory":None}
+    def __init__(self, config) -> None:
+        self.shared_memory = {"long_term_memory": [], "short_term_memory": None}
         self.agents = None
-        
+
         self.summary_system_prompt = {}
         self.summary_last_prompt = {}
         self.environment_prompt = {}
-        for state_name,state_dict in config["states"].items():
-            if state_name!="end_state":
-            
-                self.summary_system_prompt[state_name] = state_dict["summary_system_prompt"] if "summary_system_prompt" in state_dict else  "\nYour task is to summarize the historical dialogue records according to the current scene, and summarize the most important information"
-                
-                self.summary_last_prompt[state_name] = state_dict["summary_last_prompt"] if "summary_last_prompt" in state_dict else  "Please make a summary based on the historical chat records, the output format is history summary: \{your summary content\} "
-                
-                self.environment_prompt[state_name] = state_dict["environment_prompt"] if "environment_prompt" in state_dict else  " "
-                LLM_type = state_dict['LLM_type'] if "LLM_type" in state_dict else "OpenAI"
+
+        # 初始化每个state 的summary 方法
+        # Initialize the summary method for each state
+        for state_name, state_dict in config["states"].items():
+            if state_name != "end_state":
+                self.summary_system_prompt[state_name] = (
+                    state_dict["summary_system_prompt"]
+                    if "summary_system_prompt" in state_dict
+                    else "\nYour task is to summarize the historical dialogue records according to the current scene, and summarize the most important information"
+                )
+
+                self.summary_last_prompt[state_name] = (
+                    state_dict["summary_last_prompt"]
+                    if "summary_last_prompt" in state_dict
+                    else "Please make a summary based on the historical chat records, the output format is history summary: \{your summary content\} "
+                )
+
+                self.environment_prompt[state_name] = (
+                    state_dict["environment_prompt"]
+                    if "environment_prompt" in state_dict
+                    else " "
+                )
+                LLM_type = (
+                    state_dict["LLM_type"] if "LLM_type" in state_dict else "OpenAI"
+                )
                 if LLM_type == "OpenAI":
-                    self.LLM = OpenAILLM(**state_dict["LLM"])
+                    if "LLM" in state_dict:
+                        self.LLM = OpenAILLM(**state_dict["LLM"])
+                    else:
+                        self.LLM = OpenAILLM()
         self.roles_to_names = None
-        self.names_to_roles = None           
-    
+        self.names_to_roles = None
+
     @classmethod
-    def from_config(cls,config_path):
-        
+    def from_config(cls, config_path):
         with open(config_path) as f:
             config = json.load(f)
         return cls(config)
-        
-    
-    def summary(self,current_state):
+
+    def summary(self, current_state):
+        """
+        Summarize the situation in the current environment every once in a while
+        """
+        MAX_CHAT_HISTORY = eval(os.environ["MAX_CHAT_HISTORY"])
         current_state_name = current_state.name
 
-        # get relevant memory
         query = self.shared_memory["long_term_memory"][-1]
         key_history = get_key_history(
             query,
@@ -45,39 +66,54 @@ class Environment:
         )
 
         relevant_history = Memory.get_chat_history(key_history)
-        chat_history = Memory.get_chat_history(self.shared_memory["long_term_memory"])
-        summary = self.shared_memory["short_term_memory"]
-
-        current_memory = f"The information you need to know is as follows:\n<information>\nThe summary of the previous dialogue history is:<summary>\n{summary}\n.The latest conversation record is as follows:\n<hisroty> {chat_history}\n<history>,the relevant chat history you may need is:<relevant>{relevant_history}<relevant>"
-
-        system_prompt = self.environment_prompt[current_state_name] + current_memory + self.summary_system_prompt[current_state_name]
-        response = self.LLM.get_response(
-            None,
-            system_prompt,
-            stream=False
+        chat_history = Memory.get_chat_history(
+            self.shared_memory["long_term_memory"][-MAX_CHAT_HISTORY + 1 :]
         )
+        summary = self.shared_memory["short_term_memory"]
+        
+        # current_memory = summary + chat history + relevant history
+        current_memory = f"The information you need to know is as follows:\n<information>\n\
+            The summary of the previous dialogue history is:<summary>\n{summary}\n.\
+            The latest conversation record is as follows:\n<hisroty> {chat_history}\n<history>,\
+            the relevant chat history you may need is:<relevant>{relevant_history}<relevant>"
+
+        # system prompt = environment prompt + current memory + system prompt
+        system_prompt = (
+            self.environment_prompt[current_state_name]
+            + current_memory
+            + self.summary_system_prompt[current_state_name]
+        )
+        response = self.LLM.get_response(None, system_prompt, stream=False)
         return response
-    
-    def excute_action(self,action):
+
+    def excute_action(self, action):
+        """
+        get memory by action
+        """
         response = action["response"] if "response" in action else ""
         res_dict = action["res_dict"] if "res_dict" in action else {}
-        is_user =  action["is_user"] if "is_user" in action else False
+        is_user = action["is_user"] if "is_user" in action else False
         send_name = action["name"]
         send_role = action["role"]
         all = ""
         for res in response:
             all += res
         parse = f"{send_name}:"
-        if parse in all:
+        
+        # 将里面对话的第三人称删了
+        # The third person in the dialogue was deleted.
+        while parse in all:
             index = all.index(parse) + len(parse)
             all = all[index:]
         if not is_user:
             print(f"{send_name}({send_role}):{all}")
-        memory = Memory(send_role,send_name,all)
+        memory = Memory(send_role, send_name, all)
         return memory
-    
-    
-    def update_memory(self, memory,current_state):
+
+    def update_memory(self, memory, current_state):
+        """
+        update chat embbedings and long term memory,short term memory,agents long term memory
+        """
         MAX_CHAT_HISTORY = eval(os.environ["MAX_CHAT_HISTORY"])
         self.shared_memory["long_term_memory"].append(memory)
         current_embedding = get_embedding(memory.content)
@@ -91,6 +127,5 @@ class Environment:
         if len(self.shared_memory["long_term_memory"]) % MAX_CHAT_HISTORY:
             summary = self.summary(current_state)
             self.shared_memory["short_term_memory"] = summary
-        
-        self.agents[memory.send_name].update_memory(memory)
 
+        self.agents[memory.send_name].update_memory(memory)
--- src/agents/SOP.py
+++ src/agents/SOP.py
@@ -17,61 +17,96 @@
 import random
 from LLMs.base_LLM import *
 from State import State
-from utils import extract,get_key_history
+from utils import extract, get_key_history
 from Memorys import Memory
 import json
+
+
 class SOP:
     """
     input:the json of the sop
-    sop indispensable attribute: begin_role   begin_query begin_query states(agent_states)  relation
+    sop indispensable attribute: "states"  "relations"  "root"
 
     output: a sop graph
     """
 
+    # SOP should have args : "states" "relations" "root"
+
     def __init__(self, **kwargs):
-        
         self.controller_dict = {}
-        LLM_type = kwargs['LLM_type'] if "LLM_type" in kwargs else "OpenAI"
+        LLM_type = kwargs["LLM_type"] if "LLM_type" in kwargs else "OpenAI"
         if LLM_type == "OpenAI":
-            self.LLM = OpenAILLM(**kwargs["LLM"])
-            
+            self.LLM = (
+                OpenAILLM(**kwargs["LLM"])
+                if "LLM" in kwargs
+                else OpenAILLM(log_path="logs/上帝")
+            )
+
         self.states = {}
         self.init_states(kwargs["states"])
         self.init_relation(kwargs["relations"])
-        for state_name,states_dict in kwargs["states"].items():
-            if state_name != "end_state":
+        for state_name, states_dict in kwargs["states"].items():
+            if state_name != "end_state" and "controller" in states_dict:
                 self.controller_dict[state_name] = states_dict["controller"]
-            
-            
-        self.user_roles = kwargs["user_roles"] if "user_roles" in kwargs else []
+
+        self.user_names = kwargs["user_names"] if "user_names" in kwargs else []
         self.root = self.states[kwargs["root"]]
         self.current_state = self.root
-        self.finish_state_name =  kwargs["finish_state_name"] if "finish_state_name" in kwargs else "end_state"
+        self.finish_state_name = (
+            kwargs["finish_state_name"]
+            if "finish_state_name" in kwargs
+            else "end_state"
+        )
         self.roles_to_names = None
         self.names_to_roles = None
         self.finished = False
+        self.current_chat_history_idx = 0
 
     @classmethod
-    def from_config(cls,config_path):
+    def from_config(cls, config_path):
         with open(config_path) as f:
             config = json.load(f)
         sop = SOP(**config)
         return sop
-    
-    
-    def init_states(self,states_dict):
-        for state_name,state_dict in states_dict.items():
+
+    def init_states(self, states_dict):
+        for state_name, state_dict in states_dict.items():
             self.states[state_name] = State(**state_dict)
-    
-    def init_relation(self,relations):
-        for state_name,state_relation in relations.items():
-            for idx , next_state_name in state_relation.items():
-                self.states[state_name].next_states[idx] = self.states[next_state_name] 
-                
-    def transit(self, chat_history,**kwargs):
-        
+
+    def init_relation(self, relations):
+        for state_name, state_relation in relations.items():
+            for idx, next_state_name in state_relation.items():
+                self.states[state_name].next_states[idx] = self.states[next_state_name]
+
+    def transit(self, chat_history, **kwargs):
+        """
+        Determine the next state based on the current situation
+        """
         current_state = self.current_state
         controller_dict = self.controller_dict[current_state.name]
+
+        controller_type = (
+            self.controller_dict[current_state.name]["controller_type"]
+            if "controller_type" in self.controller_dict[current_state.name]
+            else "rule"
+        )
+        max_cycle_num = (
+            self.controller_dict[current_state.name]["max_cycle_num"]
+            if "max_cycle_num" in self.controller_dict[current_state.name]
+            else None
+        )
+
+        # 如果是顺序发言且有最大发言轮次的话，到达最大发言轮次则直接结束
+        # If speaking in sequence and there is a max_cycle_num, it will end directly when the max_cycle_num is reached
+        if (
+            controller_type == "order"
+            and max_cycle_num
+            and current_state.cycle_num >= max_cycle_num
+        ):
+            return "1"
+
+        # 否则则让controller判断是否结束
+        # Otherwise, let the controller judge whether to end
         system_prompt = (
             "<environment>"
             + current_state.environment_prompt
@@ -79,25 +114,41 @@ class SOP:
             + controller_dict["judge_system_prompt"]
         )
 
-        judge_last_prompt = controller_dict["judge_last_prompt"]
+        last_prompt = controller_dict["judge_last_prompt"]
         environment = kwargs["environment"]
         summary = environment.shared_memory["short_term_memory"]
-        
-        last_prompt = f"The previous summary of chat history is as follows :<summary>\n{summary}\n<summary>.The new chat history is as follows:\n<new chat> {Memory.get_chat_history(chat_history)}\n<new chat>\n<information>.\nYou especially need to pay attention to the last query<query>\n{chat_history[-1].content}\n<query>\n{judge_last_prompt}\n"
-        
+
+        chat_messages = [
+            {
+                "role": "user",
+                "content": f"The previous summary of chat history is as follows :<summary>\n{summary}\n<summary>.The new chat history is as follows:\n<new chat> {Memory.get_chat_history(chat_history)}\n<new chat>\n<information>.\nYou especially need to pay attention to the last query<query>\n{chat_history[-1].content}\n<query>\n",
+            }
+        ]
         extract_words = controller_dict["judge_extract_words"]
+
         response = self.LLM.get_response(
-            None, system_prompt, last_prompt, stream=False, **kwargs
+            chat_messages, system_prompt, last_prompt, stream=False, **kwargs
         )
-        next_state = response if response.isdigit() else extract(response, extract_words)
+        next_state = (
+            response if response.isdigit() else extract(response, extract_words)
+        )
         return next_state
 
-    def route(self, current_state,chat_history,**kwargs):
+    def route(self, chat_history, **kwargs):
+        """
+        Determine the role that needs action based on the current situation
+        """
+        # 获取当前状态下的控制器
+        # Get the current state of the controller
+        current_state = self.current_state
         controller_type = (
             self.controller_dict[current_state.name]["controller_type"]
             if "controller_type" in self.controller_dict[current_state.name]
             else "rule"
         )
+
+        # 如果是rule 控制器，则交由LLM进行分配角色
+        # If  controller type is rule, it is left to LLM to assign roles.
         if controller_type == "rule":
             controller_dict = self.controller_dict[current_state.name]
             system_prompt = (
@@ -107,45 +158,110 @@ class SOP:
                 + controller_dict["call_system_prompt"]
             )
 
+            # last_prompt: note + last_prompt + query
+            last_prompt = (
+                f"You especially need to pay attention to the last query<query>\n{chat_history[-1].content}\n<query>\n"
+                + controller_dict["call_last_prompt"]
+                + f"Note: The person whose turn it is now cannot be the same as the person who spoke last time, so <end>{chat_history[-1].send_name}</end> cannot be output\n."
+            )
 
-            chat_messages = Memory.get_chat_history(chat_history)
-            call_last_prompt = controller_dict["call_last_prompt"]
-            last_prompt = f"The chat history is as follows:\n<history>\n{chat_messages}<history>\n，The last person to speak is: {chat_history[-1].send_name}\n.{call_last_prompt}Note: The person whose turn it is now cannot be the same as the person who spoke last time, so <end>{chat_history[-1].send_name}</end> cannot be output,You especially need to pay attention to the last query<query>\n{chat_history[-1].content}\n<query>\n"
-            
+            # Intermediate historical conversation records
+            chat_messages = [
+                {
+                    "role": "user",
+                    "content": f"The chat history is as follows:\n<history>\n{Memory.get_chat_history(chat_history)}<history>\n，\
+                    The last person to speak is: {chat_history[-1].send_name}\n.",
+                }
+            ]
 
             extract_words = controller_dict["call_extract_words"]
-            
-            response = self.LLM.get_response(
-                None, system_prompt, last_prompt, stream=False,**kwargs
-            )
-            
-            next_role = extract(response, extract_words)
-            
-        elif controller_type == "order":
-            if not current_state.current_role:
-                next_role = current_state.roles[0]
-            else:
-                index = current_state.roles.index(current_state.current_role)
-                next_role = current_state.roles[(index + 1) % len(current_state.roles)]
 
+            response = self.LLM.get_response(
+                chat_messages, system_prompt, last_prompt, stream=False, **kwargs
+            )
+
+            # get next role
+            next_role = extract(response, extract_words)
+
+        # Speak in order
+        elif controller_type == "order":
+            # If there is no begin role, it will be given directly to the first person.
+            if current_state.current_role:
+                next_role = current_state.roles[0]
+            # otherwise first
+            else:
+                current_state.index += 1
+                next_role = current_state.roles[
+                    (current_state.index) % len(current_state.roles)
+                ]
+                if current_state.index == current_state.begin_index:
+                    current_state.cycle_num += 1
+                    
+        # random speak
         elif controller_type == "random":
             next_role = random.choice(current_state.roles)
 
         return next_role
 
-    def next(self,environment,agents):
-        if self.current_state.is_begin:
-            current_state = self.root
-            current_agent_name = self.roles_to_names[current_state.name][current_state.begin_role]
-            memory =Memory(current_state.begin_role,current_agent_name,current_state.begin_query)
-            print(f"{current_agent_name}({current_state.begin_role}):{current_state.begin_query}")
-            environment.update_memory(memory,current_state)
-            self.current_state.is_begin = False
-            return self.next(environment,agents)
+    def next(self, environment, agents):
+        """
+        Determine the next state and the role that needs action based on the current situation
+        """
         
+        # 如果是第一次进入该状态
+        # If it is the first time to enter this state
+        if self.current_state.is_begin:
+            
+            # 该状态设为非第一次进入,并将聊天记录更新至新状态
+            # This state is set to not be the first entry, and the chat history is updated to the new state
+            self.current_state.is_begin = False
+            print(f"{self.current_state.name}")
+            self.current_chat_history_idx = (
+                len(environment.shared_memory["long_term_memory"]) - 1
+            )
+            current_state = self.current_state
+
+            # 如果该状态下有开场白，则进行以下操作
+            # If there is an opening statement in this state, do the following
+            if current_state.begin_role:
+                current_state.current_role = current_state.begin_role
+                current_agent_name = self.roles_to_names[current_state.name][
+                    current_state.begin_role
+                ]
+
+                # 找出当前的agent
+                # Find out the current agent
+                current_agent = agents[current_agent_name]
+                
+                # 如果是用户的话,则用户负责输入开场白
+                # If it is a user, the user is responsible for entering the begin query
+                if current_agent.is_user:
+                    current_state.begin_query = input(f"{current_agent_name}:")
+                    
+                # Otherwise, enter a preset begin query
+                else:
+                    print(
+                        f"{current_agent_name}({current_state.begin_role}):{current_state.begin_query}"
+                    )
+
+                # 将开场白更新至记忆
+                # Update begin query to memory
+                memory = Memory(
+                    current_state.begin_role,
+                    current_agent_name,
+                    current_state.begin_query,
+                )
+                environment.update_memory(memory, current_state)
+
         current_state = self.current_state
+
+        # 如果是单一循环节点，则一直循环即可
+        # If it is a single loop node, just keep looping
         if len(current_state.next_states) == 1:
             next_state = "0"
+
+        # 否则则需要controller去判断进入哪一节点
+        # Otherwise, the controller needs to determine which node to enter.
         else:
             query = environment.shared_memory["long_term_memory"][-1]
 
@@ -154,46 +270,59 @@ class SOP:
                 environment.shared_memory["long_term_memory"][:-1],
                 environment.shared_memory["chat_embeddings"][:-1],
             )
+
+            # 只传入当前状态下的对话记录
+            # Only pass in the conversation record in the current state
             next_state = self.transit(
-                chat_history=environment.shared_memory["long_term_memory"],
+                chat_history=environment.shared_memory["long_term_memory"][
+                    self.current_chat_history_idx :
+                ],
                 key_history=key_history,
-                environment_prompt=current_state.environment_prompt,
-                environment= environment
+                environment=environment,
             )
 
+        # 如果没有parse出来则继续循环
+        # If no parse comes out, continue looping
         if not next_state.isdigit():
             next_state = "0"
+
+        # 如果进入终止节点，则直接终止
+        # If you enter the termination node, terminate directly
         if self.current_state.next_states[next_state].name == self.finish_state_name:
             self.finished = True
-            return None , None
-        next_state = current_state.next_states[next_state]
+            return None, None
+
+        self.current_state = current_state.next_states[next_state]
+        current_state = self.current_state
         
+        # 知道进入哪一状态后开始分配角色，如果该状态下只有一个角色则直接分配给他
+        # Start assigning roles after knowing which state you have entered. If there is only one role in that state, assign it directly to him.
         if len(current_state.roles) == 1:
-            next_role = current_state.roles[0]
+            current_role = current_state.roles[0]
+
+        # 否则controller进行分配
+        # Otherwise the controller determines
         else:
             query = environment.shared_memory["long_term_memory"][-1]
-               
+
             key_history = get_key_history(
                 query,
                 environment.shared_memory["long_term_memory"][:-1],
                 environment.shared_memory["chat_embeddings"][:-1],
             )
-            next_role = self.route(
-                current_state=next_state,
-                chat_history=environment.shared_memory["long_term_memory"],
+            current_role = self.route(
+                chat_history=environment.shared_memory["long_term_memory"][
+                    self.current_chat_history_idx :
+                ],
                 key_history=key_history,
-                environment_prompt=current_state.environment_prompt,
             )
 
-        if next_role not in next_state.roles:
-            next_role = random.choice(next_state.roles)
-        
-        is_user = next_role in self.user_roles
+        # 如果下一角色不在，则随机挑选一个
+        # If the next character is not available, pick one at random
+        if current_role not in current_state.roles:
+            current_role = random.choice(current_state.roles)
 
-        self.current_state = next_state
-        
-        current_agent = agents[self.roles_to_names[next_state.name][next_role]]
-        current_agent.is_user = is_user
-        current_state.current_role = next_role
+        current_agent = agents[self.roles_to_names[current_state.name][current_role]]
+        current_state.current_role = current_role
 
-        return current_state,current_agent
+        return current_state, current_agent
