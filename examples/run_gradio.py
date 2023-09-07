10726913ddef8a47b68fef15d6b267717a228396
Jintian Zhang <65744560+MikeDean2367@users.noreply.github.com>
2023-09-07 17:51:59 +0800

    Create run_gradio.py

---
+++ examples/run_gradio.py
@@ -0,0 +1,15 @@
+# SingleAgent
+import sys
+sys.path.append("../../cfg")
+
+from gradio_example import SingleAgentUI
+
+if __name__ == '__main__':
+    # 启动client_server_file并自动传递消息
+    ui = DebateUI(client_server_file="serving.py")
+    # 构建映射关系
+    # GradioConfig.add_agent(agents_name=ui.all_agents_name)
+    # 搭建前端并建立监听事件
+    ui.construct_ui()
+    # 启动运行
+    ui.run(share=True)
