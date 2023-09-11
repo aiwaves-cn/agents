2f81857ddbbc082a8ba8708ccbfd6cab730592a9
Jintian Zhang <65744560+MikeDean2367@users.noreply.github.com>
2023-09-11 13:51:10 +0800

    Update gradio_example.py

--- examples/cfg/gradio_example.py
+++ examples/cfg/gradio_example.py
@@ -428,6 +428,7 @@ class SingleAgentUI(WebUI):
             assert False
         # print("MIKE-data_history", self.data_history)
         # 如果state%10==2，说明到了新节点
+        print("123:", history)
         render_data = self.render_bubble(history, self.data_history, node_name, render_node_name= state % 10 == 2)
         return render_data
 
@@ -457,6 +458,7 @@ class SingleAgentUI(WebUI):
                         gr.Textbox.update(visible=True, interactive=True)
                     return
                 else:
+                    print("1234:", history)
                     history = self.handle_message(history, state, agent_name, token, node_name)
                     yield history, \
                           gr.Button.update(visible=False, interactive=False), \
@@ -481,8 +483,7 @@ class SingleAgentUI(WebUI):
             gr.Textbox.update(value="", visible=True, interactive=True), \
                 gr.Button.update(visible=True, interactive=True),\
                     gr.Button.update(visible=True, interactive=True, value="重启")
-        
-     
+           
     def prepare(self):
         if self.FIRST:
             self.send_start_cmd()
@@ -513,8 +514,10 @@ class SingleAgentUI(WebUI):
     ):
         """从cache中判断一下是否是用户输入第一句话"""
         content = None
+        print("hi::",self.cache["user_first"])
         if not self.cache["user_first"]:
             content = self.prepare()
+        print("hi:",content is None)
         with gr.Blocks(css=gc.CSS) as demo:
             with gr.Column():
                 self.chatbot = gr.Chatbot(
@@ -572,35 +575,251 @@ class SingleAgentUI(WebUI):
 
 class NovelUI(WebUI):
     def __init__(
-            self,
-            client_server_file: str,
-            socket_host: str = '127.0.0.1',
-            socket_port: int = 9999,
-            bufsize: int = 1024
+        self,
+        client_cmd: list,
+        socket_host: str = HOST,
+        socket_port: int = PORT,
+        bufsize: int = 1024
     ):
-        super(NovelUI, self).__init__(client_server_file, socket_host, socket_port, bufsize)
+        super(NovelUI, self).__init__(client_cmd, socket_host, socket_port, bufsize)
 
+class CodeUI(WebUI):
+    """传给我，我传给他，然后启动"""
+    
+    def render_and_register_ui(self):
+        self.agent_name:list = [self.cache["agents_name"]] if isinstance(self.cache["agents_name"], str) else self.cache['agents_name']
+        gc.add_agent(self.agent_name)
+    
+    def __init__(
+        self,
+        client_cmd: list,
+        socket_host: str = HOST,
+        socket_port: int = PORT,
+        bufsize: int = 1024 
+    ):
+        super(CodeUI, self).__init__(client_cmd, socket_host, socket_port, bufsize)
+        self.first_recieve_from_client()
+        self.data_history = list()
+    
+    def construct_ui(self):
+        with gr.Blocks(css=gc.CSS) as demo:
+            with gr.Row():
+                # 第一列
+                with gr.Column():
+                    self.chatbot = gr.Chatbot(
+                        elem_id="chatbot1"
+                    )
+                    with gr.Row():
+                        self.text_requirement = gr.Textbox(
+                            value=self.cache['requirement'],
+                            placeholder="请输入你的要求",
+                            scale=9,
+                        )
+                        self.btn_start = gr.Button(
+                            value="开始",
+                            scale=1
+                        )
+                    self.btn_reset = gr.Button(
+                        value="重启",
+                        visible=False
+                    )
+                
+                # 第二列
+                with gr.Column():
+                    self.file = gr.File(visible=False)
+                    self.chat_code_show = gr.Chatbot(
+                        elem_id="chatbot1",
+                        visible=False
+                    )   
+            # =========创建监听事件==========
+            self.btn_start.click(
+                fn=self.btn_send_when_click,
+                inputs=[self.chatbot, self.text_requirement],
+                outputs=[self.chatbot, self.btn_start, self.text_requirement, self.btn_reset]
+            ).then(
+                fn=self.btn_send_after_click,
+                inputs=[self.file, self.chatbot, self.chat_code_show, self.btn_start, self.btn_reset, self.text_requirement],
+                outputs=[self.file, self.chatbot, self.chat_code_show, self.btn_start, self.btn_reset, self.text_requirement]
+            )
+            self.text_requirement.submit(
+                fn=self.btn_send_when_click,
+                inputs=[self.chatbot, self.text_requirement],
+                outputs=[self.chatbot, self.btn_start, self.text_requirement, self.btn_reset]
+            ).then(
+                fn=self.btn_send_after_click,
+                inputs=[self.file, self.chatbot, self.chat_code_show, self.btn_start, self.btn_reset, self.text_requirement],
+                outputs=[self.file, self.chatbot, self.chat_code_show, self.btn_start, self.btn_reset, self.text_requirement]
+            )
+            self.btn_reset.click(
+                # 更新UI
+                fn=self.btn_reset_when_click,
+                inputs=[],
+                outputs=[self.file, self.chatbot, self.chat_code_show, self.btn_start, self.btn_reset, self.text_requirement]
+            ).then(
+                # 设置监听事件
+                fn=self.btn_reset_after_click,
+                inputs=[self.file, self.chatbot, self.chat_code_show, self.btn_start, self.btn_reset, self.text_requirement],
+                outputs=[self.file, self.chatbot, self.chat_code_show, self.btn_start, self.btn_reset, self.text_requirement]
+            )
+            self.file.select(
+                fn=self.file_when_select,
+                inputs=[self.file],
+                outputs=[self.chat_code_show]
+            )
+            # ==============================
+            self.demo = demo
+    
+    # 监听
+    def btn_send_when_click(self, chatbot, text_requirement):
+        """
+        inputs=[self.chatbot, self.text_requirement],
+        outputs=[self.chatbot, self.btn_start, self.text_requirement, self.btn_reset]
+        """
+        """渲染气泡，然后隐藏按钮"""
+        chatbot = [[UIHelper.wrap_css(content=text_requirement, name="User"), None]]
+        yield chatbot,\
+            gr.Button.update(visible=True, interactive=False, value="运行中"),\
+            gr.Textbox.update(visible=True, interactive=False, value=""),\
+            gr.Button.update(visible=True)
+        """发送启动命令"""
+        self.send_start_cmd({'requirement': text_requirement})
+    
+    def btn_send_after_click(
+        self, 
+        file,
+        chatbot,
+        show_code,
+        btn_send,
+        btn_reset,
+        text_requirement
+    ):
+        """这个应该就自动运行到结束"""
+        """更新chatbot"""
+        """结束的state为99"""
+        self.data_history = list()
+        receive_server = self.receive_server
+        while True:
+            data_list: List = receive_server.send(None)
+            print("收到:", data_list)
+            for item in data_list:
+                data = eval(item)
+                assert isinstance(data, list)
+                state, agent_name, token, node_name = data
+                assert isinstance(state, int)
+                """非人机"""
+                assert state in [10, 11, 12, 99]
+                if state == 99:
+                    """结束渲染"""
+                    yield None, None, None, None, None, None
+                    return
+                history = self.handle_message(history, state, agent_name, token, node_name)
+                yield gr.File.update(visible=False),\
+                    history, \
+                    gr.Chatbot.update(visible=False),\
+                    gr.Button.update(visible=True, interactive=False),\
+                    gr.Button.update(visible=True, interactive=True),\
+                    gr.Textbox.update(visible=True, interactive=False)
+    
+    def btn_reset_when_click(self):
+        """
+        inputs = []
+        outputs = [self.file, self.chatbot, self.chat_code_show, self.btn_start, self.btn_reset, self.text_requirement]
+        """
+        return gr.File.update(visible=False),\
+            None, None, gr.Button.update(value="重启中", interactive=False),\
+                gr.Button.update(value="重启中", interactive=False),\
+                    gr.Textbox.update(value="重启中", interactive=False)
+    
+    def btn_reset_after_click(
+        self,
+        file,
+        chatbot,
+        show_code,
+        btn_send,
+        btn_reset,
+        text_requirement
+    ):
+        self.reset()
+        """接受来自client的值"""
+        self.first_recieve_from_client(reset_mode=True)
+        return gr.File.update(value=None, visible=False),\
+            gr.Chatbot.update(value=None, visible=True),\
+            gr.Chatbot.update(value=None, visible=False),\
+            gr.Button.update(value="开始", visible=True, interactive=True),\
+            gr.Button.update(value="重启", interactive=False, visible=False),\
+            gr.Textbox.update(value="", interactive=True, visible=True)
+        
+    """监听文件点击"""
+    def file_when_select(self, file):
+        """点击文件放到前端去渲染"""
+        CODE_PREFIX = "```python\n{}\n```"
+        with open(file.name, "r", encoding='utf-8') as f:
+            contents = f.readlines()
+        codes = "".join(contents)
+        return [[CODE_PREFIX.format(codes),None]]
+        
+        
 def test_ui():
-    CSS ="""
-.contain { display: flex; flex-direction: column; }
-.gradio-container { height: 100vh !important; }
-.full-height {
-    height: 100vh; 
-}
-#component-0 { height: 100%; }
-#chatbot1 { flex-grow: 1; overflow: auto;}
+    # with gr.Blocks(css=gc.CSS) as demo:
+    #     with gr.Row():
+    #         with gr.Column():
+    #             # 实时对话
+    #             chatbot1 = gr.Chatbot(
+    #                 elem_id="chatbot1",
+    #                 label="对话",
+    #             )
+    #             # 输入的要求
+    #             text_requirement = gr.Textbox(
+    #                 placeholder="剧本的要求",
+    #                 value=""
+    #             )
+    #             with gr.Row():
+    #                 radio_start_agent = gr.Radio(
+    #                     label="开始的agent",
+    #                     choices=[str(i) for i in range(5)]
+    #                 )
+    #                 text_start_query = gr.Textbox(
+    #                     value="开始的query",
+    #                     placeholder=""
+    #                 )
+    code = """```python
+import torch
+class A:
+    def __init__(self):
+        print("class A is instanced")
+        
+if __name__ == '__main__':
+    a = A()
+    print("done")  
+```  
 """
-    # gc.CSS
-    with gr.Blocks(css=CSS) as demo:
+    with gr.Blocks(css=gc.CSS) as demo:
         with gr.Row():
-            chatbot1 = gr.Chatbot(
-                # elem_id="chatbot1",
-                label="对话",
-            )
-            chatbot2 = gr.Chatbot(
-                # elem_id="chatbot1",
-                label="对话",
-            )
+            with gr.Column():
+                chatbot = gr.Chatbot(
+                    elem_id="chatbot1"
+                )
+                with gr.Row():
+                    text_requirement = gr.Textbox(
+                        value="",
+                        placeholder="请输入你的要求",
+                        scale=9
+                    )
+                    btn_start = gr.Button(
+                        value="开始",
+                        scale=1
+                    )
+                btn_reset = gr.Button(
+                    value="重启"
+                )
+                
+            with gr.Column():
+                file = gr.File()
+                chat_code_show = gr.Chatbot(
+                    elem_id="chatbot1",
+                    value=[[code, None]]
+                )            
+            
             
     demo.queue()
     demo.launch(share=True)
