038162c1b51504977bd98fb21bc986ab1c4d7a67
jianghuyihei <2504702369@qq.com>
2023-09-12 18:29:39 +0800

    fix bugs

--- examples/run_gradio.py
+++ examples/run_gradio.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 sys.path.append("cfg")
 
@@ -5,9 +6,12 @@ from gradio_example import SingleAgentUI
 
 if __name__ == '__main__':
     # 启动client_server_file并自动传递消息
+    parser = argparse.ArgumentParser(description='A demo of chatbot')
+    parser.add_argument('--agent', type=str, help='path to SOP json')
+    args = parser.parse_args()
     ui = SingleAgentUI(
         # client_server_file="serving.py"
-        client_cmd=["python", "gradio_backend.py"]
+        client_cmd=["python", "gradio_backend.py","--agent",args.agent]
     )
     # 构建映射关系
     # GradioConfig.add_agent(agents_name=ui.all_agents_name)
