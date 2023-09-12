4f466ebc8532da717ccadc8e0d167f57b0dcd3aa
jianghuyihei <2504702369@qq.com>
2023-09-12 14:09:21 +0800

    fix bugs

--- src/agents/Prompt/base_Prompts.py
+++ src/agents/Prompt/base_Prompts.py
@@ -26,7 +26,7 @@ Allocate_component = "f\"If it's currently supposed to be speaking for {role}, t
 Call_system_prompt = "f\"{environment_prompt};{call_system_prompt};{allocate_prompt}\""
 
 #
-Call_last_prompt = "f\"You especially need to pay attention to the last query<query>\\n{query}\\n</query> and the relevant conversation <relevant>\\n{relevant_history} \\n</relevant>\\n;{allocate_prompt};Note: The person whose turn it is now cannot be the same as the person who spoke last time, so {last_name} cannot be output\\n.\""
+Call_last_prompt = "f\"You especially need to pay attention to the last query<query>\\n{query}\\n</query> and the relevant conversation <relevant>\\n{relevant_history} \\n</relevant>\\n;Now please choose the person to speak according to the following rules :{allocate_prompt};Note: The person whose turn it is now cannot be the same as the person who spoke last time, so {last_name} cannot be output\\n.\""
 
 Call_message = "f\"The chat history is as follows:\\n<history>\\n{chat_history_message}</history>\\n;The last person to speak is: {last_name}\\n. \""
 #sop.call================================================================
@@ -67,7 +67,7 @@ Agent_observe_relevant_memory = "f\"The relevant chat history are as follows:\\n
 
 Agent_observe_memory = "f\"Here's what you need to know(Remember, this is just information, Try not to repeat what's inside):\\n<information>\\n{relevant_memory};\
             The previous summary of chat history is as follows :<summary>\\n{agent.short_term_memory}\\n</summary>.\
-            The new chat history is as follows:\\n<new chat> {conversations}\\n</new chat>\\n\
+            The new chat history is as follows:\\n<history> {conversations}\\n</history>\\n\
             </information>\""
 # Environment========================================================================================================
 
@@ -77,7 +77,7 @@ Agent_observe_memory = "f\"Here's what you need to know(Remember, this is just i
 # Agent========================================================================================================
 Agent_summary_system_prompt = "f\"{summary_prompt};Please summarize past key summary \\n<summary>\\n {self.short_term_memory} </summary>and new chat_history as follows: <new chat>\\n{conversations}</new chat>\""          
             
-Agent_last_prompt = "f\"{last_prompt};\\n{query.send_name} said that: {query.content};Please try to respond differently from your existing knowledge, and avoid repeating what others have said.Please just talk as normal.\""
+Agent_last_prompt = "f\"{last_prompt};\\nPlease continue the talk based on your known information;Please try to respond differently from your existing knowledge, and avoid repeating what others have said.Please just talk as normal.\""
 
 Agent_system_prompt = "f\"{system_prompt},\""
 # Agent========================================================================================================
